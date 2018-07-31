9d1b92da3a77e6c9a2de3730fc7679283529475a
Nyaasu <nyaasu@qq.com>
2018-07-31 15:55:23 +0800

    Page_2_v1.3

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -16,8 +16,9 @@ var start = function() {
         //云层离开效果
         document.getElementById('cloud-left').style.animation = 'cloudFadeOut 1s 0.3s'
         document.getElementById('cloud-left').style.animationFillMode = 'forwards'
-        document.getElementById('cloud-right').style.animation = 'cloudFadeOut 1s 0.35s'
+        document.getElementById('cloud-right').style.animation = 'cloudFadeOut 1s 0.37s'
         document.getElementById('cloud-right').style.animationFillMode = 'forwards'
+        //菜单生成
         setTimeout(function() {
           var guideUrl = ['1','2','3','4']
           var guideName = ['入校事宜','学业指导','校园文化','乐居香樟']
