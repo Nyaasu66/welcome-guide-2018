4ccd50e4f4cbf7417b0c1439ebcf737db5904951
Nyaasu <nyaasu@qq.com>
2018-08-02 09:22:23 +0800

    Removed touch-action: none frrom public.css

--- public/javascripts/guide.js
+++ public/javascripts/guide.js
@@ -1,60 +1,47 @@
-var start = function() {
+var start = function () {
 	changeSize()
 	var pageChange = document.getElementById('page-change'),
-			pageChangeUl = document.getElementById('page-change-ul'),
-			hash = parseInt(location.hash.split('#')[1]),
-			pageWidth = pageChange.offsetWidth,
-			unit = 100 * (parseFloat(document.getElementsByTagName('html')[0].style.fontSize) / 625 / 0.16),
-			canChange = true
-	pageChangeUl.getElementsByTagName('li')[hash+1].className = 'now-page'
+		pageChangeUl = document.getElementById('page-change-ul'),
+		hash = parseInt(location.hash.split('#')[1]),
+		pageWidth = pageChange.offsetWidth,
+		unit = 100 * (parseFloat(document.getElementsByTagName('html')[0].style.fontSize) / 625 / 0.16),
+		canChange = true
+	pageChangeUl.getElementsByTagName('li')[hash + 1].className = 'now-page'
 	pageChangeUl.style.left = (pageWidth / 2) - (hash + 1.5) * parseFloat(pageChangeUl.getElementsByTagName('li')[0].style.width) * unit + 'px'
-  document.getElementById('page-content-' + hash).style.display = 'block'
-  $('.page-content-list li').on('touchstart', function(e) {
-    this.className = 'data-image hover'
-  })
-  $('.page-content-list li').on('touchend', function(e) {
-    this.className = 'data-image'
-  })
-  $('.page-content-list li').on('tap', function(e) {
-		if (this.getAttribute('disabled')) {
-			window.location.href = this.getAttribute('to')
-			return
-		}
-    showMengBan(parseInt(this.getAttribute('page')), parseInt(this.getAttribute('list')))
-  })
-  $('#page-content-3 .page-content-title:first').on('tap', function(e) {
-    showMengBan(parseInt(this.getAttribute('page')), parseInt(this.getAttribute('list')))
-  })
-  $('.open-site').on('tap', function(e) {
-    window.open(this.getAttribute('href'))
-  })
-	$('#switch-right').on('tap', function(e) {
-    e.stopPropagation()
+	document.getElementById('page-content-' + hash).style.display = 'block'
+	$('#page-content-3 .page-content-title:first').on('tap', function (e) {
+		showMengBan(parseInt(this.getAttribute('page')), parseInt(this.getAttribute('list')))
+	})
+	$('.open-site').on('tap', function (e) {
+		window.open(this.getAttribute('href'))
+	})
+	$('#switch-right').on('tap', function (e) {
+		e.stopPropagation()
 		console.log('right')
-		if(canChange) {
+		if (canChange) {
 			canChange = false
 			changePage(1)
 		}
 	})
-	$('#switch-left').on('tap', function(e) {
-    e.stopPropagation()
+	$('#switch-left').on('tap', function (e) {
+		e.stopPropagation()
 		console.log('left')
-		if(canChange) {
+		if (canChange) {
 			canChange = false
 			changePage(-1)
 		}
 	})
 
 
-  $('body').on('touchstart', function(e) {
-    e.stopPropagation()
-  })
+	$('body').on('touchstart', function (e) {
+		e.stopPropagation()
+	})
 
 	function jumpPage() {
 		canChange = true
-		if(pageChangeUl.getElementsByClassName('like-page')[0]) {
+		if (pageChangeUl.getElementsByClassName('like-page')[0]) {
 			var likeIndex = parseInt(pageChangeUl.getElementsByClassName('like-page')[0].getAttribute('index'))
-			switch(likeIndex) {	
+			switch (likeIndex) {
 				case 6:
 					pageChangeUl.style.transition = '0s'
 					pageChangeUl.style.left = (pageWidth / 2) - (2.5) * parseFloat(pageChangeUl.getElementsByTagName('li')[0].style.width) * unit + 'px'
@@ -74,13 +61,13 @@ var start = function() {
 	function changePage(moveX) {
 
 		var pageChangeLi = pageChangeUl.getElementsByTagName('li'),
-				nowIndex = parseInt(pageChangeUl.getElementsByClassName('now-page')[0].getAttribute('index'))
-		switch(moveX) {
+			nowIndex = parseInt(pageChangeUl.getElementsByClassName('now-page')[0].getAttribute('index'))
+		switch (moveX) {
 			case 1:
-				if(nowIndex === 5) {
-          document.getElementById('page-content-4').style.display = 'none'
+				if (nowIndex === 5) {
+					document.getElementById('page-content-4').style.display = 'none'
 					hash = 1
-          document.getElementById('page-content-1').style.display = 'block'
+					document.getElementById('page-content-1').style.display = 'block'
 					location.hash = '#' + hash
 					pageChangeLi[nowIndex].className = ''
 					pageChangeLi[6].className = 'like-page'
@@ -88,22 +75,22 @@ var start = function() {
 					pageChangeUl.style.transition = '0.1s'
 					pageChangeUl.style.left = (pageWidth / 2) - (nowIndex + 1.5) * parseFloat(pageChangeUl.getElementsByTagName('li')[0].style.width) * unit + 'px'
 				}
-				else{
-          document.getElementById('page-content-' + hash).style.display = 'none'
+				else {
+					document.getElementById('page-content-' + hash).style.display = 'none'
 					hash += 1
-          document.getElementById('page-content-' + hash).style.display = 'block'
+					document.getElementById('page-content-' + hash).style.display = 'block'
 					location.hash = '#' + hash
 					pageChangeLi[nowIndex].className = ''
-					pageChangeLi[nowIndex+1].className = 'now-page'
+					pageChangeLi[nowIndex + 1].className = 'now-page'
 					pageChangeUl.style.transition = '0.1s'
 					pageChangeUl.style.left = (pageWidth / 2) - (nowIndex + 1.5) * parseFloat(pageChangeUl.getElementsByTagName('li')[0].style.width) * unit + 'px'
-				} 
+				}
 				break
 			case -1:
-				if(nowIndex === 2) {
-          document.getElementById('page-content-1').style.display = 'none'
+				if (nowIndex === 2) {
+					document.getElementById('page-content-1').style.display = 'none'
 					hash = 4
-          document.getElementById('page-content-4').style.display = 'block'
+					document.getElementById('page-content-4').style.display = 'block'
 					location.hash = '#' + hash
 					pageChangeLi[nowIndex].className = ''
 					pageChangeLi[1].className = 'like-page'
@@ -111,34 +98,34 @@ var start = function() {
 					pageChangeUl.style.transition = '0.1s'
 					pageChangeUl.style.left = (pageWidth / 2) - (nowIndex - 0.5) * parseFloat(pageChangeUl.getElementsByTagName('li')[0].style.width) * unit + 'px'
 				}
-				else{
-          document.getElementById('page-content-' + hash).style.display = 'none'
+				else {
+					document.getElementById('page-content-' + hash).style.display = 'none'
 					hash -= 1
-          document.getElementById('page-content-' + hash).style.display = 'block'
+					document.getElementById('page-content-' + hash).style.display = 'block'
 					location.hash = '#' + hash
 					pageChangeLi[nowIndex].className = ''
-					pageChangeLi[nowIndex-1].className = 'now-page'
+					pageChangeLi[nowIndex - 1].className = 'now-page'
 					pageChangeUl.style.transition = '0.1s'
 					pageChangeUl.style.left = (pageWidth / 2) - (nowIndex - 0.5) * parseFloat(pageChangeUl.getElementsByTagName('li')[0].style.width) * unit + 'px'
-				} 
+				}
 				break
 		}
 	}
-  var dataImage = document.getElementsByClassName('data-image');
-  for(var i=0; i<dataImage.length; i++) {
-    dataImage[i].style.backgroundImage = 'url(' + dataImage[i].getAttribute('data-src') + ')';
-  }
+	var dataImage = document.getElementsByClassName('data-image');
+	for (var i = 0; i < dataImage.length; i++) {
+		dataImage[i].style.backgroundImage = 'url(' + dataImage[i].getAttribute('data-src') + ')';
+	}
 
-    function showMengBan(page,list) {
-      document.getElementById('mengban').style.display = 'flex'
-      document.getElementById('word-content').innerHTML = words[page-1][list-1]
-    }
-    function hideMengBan() {
-      document.getElementById('mengban').style.display = 'none'
-    }
-    $('#close-btn').on('touchend', function() {
-      hideMengBan()
-    })
+	function showMengBan(page, list) {
+		document.getElementById('mengban').style.display = 'flex'
+		document.getElementById('word-content').innerHTML = words[page - 1][list - 1]
+	}
+	function hideMengBan() {
+		document.getElementById('mengban').style.display = 'none'
+	}
+	$('#close-btn').on('touchend', function () {
+		hideMengBan()
+	})
 }
 
 start()
