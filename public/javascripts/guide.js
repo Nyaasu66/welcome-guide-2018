1816885fff3e04f3c809607bfb4ae1a9017906c5
Nyaasu <nyaasu@qq.com>
2018-08-02 09:51:26 +0800

    Simplified guide.js

--- public/javascripts/guide.js
+++ public/javascripts/guide.js
@@ -1,13 +1,9 @@
 var start = function () {
 	changeSize()
-	var pageChange = document.getElementById('page-change'),
-		pageChangeUl = document.getElementById('page-change-ul'),
+	var pageChangeUl = document.getElementById('page-change-ul'),
 		hash = parseInt(location.hash.split('#')[1]),
-		pageWidth = pageChange.offsetWidth,
-		unit = 100 * (parseFloat(document.getElementsByTagName('html')[0].style.fontSize) / 625 / 0.16),
 		canChange = true
 	pageChangeUl.getElementsByTagName('li')[hash + 1].className = 'now-page'
-	pageChangeUl.style.left = (pageWidth / 2) - (hash + 1.5) * parseFloat(pageChangeUl.getElementsByTagName('li')[0].style.width) * unit + 'px'
 	document.getElementById('page-content-' + hash).style.display = 'block'
 	$('#page-content-3 .page-content-title:first').on('tap', function (e) {
 		showMengBan(parseInt(this.getAttribute('page')), parseInt(this.getAttribute('list')))
@@ -15,26 +11,21 @@ var start = function () {
 	$('.open-site').on('tap', function (e) {
 		window.open(this.getAttribute('href'))
 	})
-	$('#switch-right').on('tap', function (e) {
-		e.stopPropagation()
+	$('#switch-right').on('tap', function () {
+		jumpPage()
 		console.log('right')
 		if (canChange) {
 			canChange = false
 			changePage(1)
 		}
 	})
-	$('#switch-left').on('tap', function (e) {
-		e.stopPropagation()
+	$('#switch-left').on('tap', function () {
+		jumpPage()
 		console.log('left')
 		if (canChange) {
 			canChange = false
 			changePage(-1)
 		}
-	})
-
-
-	$('body').on('touchstart', function (e) {
-		e.stopPropagation()
 	})
 
 	function jumpPage() {
@@ -43,20 +34,14 @@ var start = function () {
 			var likeIndex = parseInt(pageChangeUl.getElementsByClassName('like-page')[0].getAttribute('index'))
 			switch (likeIndex) {
 				case 6:
-					pageChangeUl.style.transition = '0s'
-					pageChangeUl.style.left = (pageWidth / 2) - (2.5) * parseFloat(pageChangeUl.getElementsByTagName('li')[0].style.width) * unit + 'px'
 					pageChangeUl.getElementsByClassName('like-page')[0].className = ''
 					break
 				case 1:
-					pageChangeUl.style.transition = '0s'
-					pageChangeUl.style.left = (pageWidth / 2) - (5.5) * parseFloat(pageChangeUl.getElementsByTagName('li')[0].style.width) * unit + 'px'
 					pageChangeUl.getElementsByClassName('like-page')[0].className = ''
 					break
 			}
 		}
 	}
-	pageChangeUl.addEventListener("webkitTransitionEnd", jumpPage);
-	pageChangeUl.addEventListener('transitionend', jumpPage)
 
 	function changePage(moveX) {
 
@@ -72,8 +57,6 @@ var start = function () {
 					pageChangeLi[nowIndex].className = ''
 					pageChangeLi[6].className = 'like-page'
 					pageChangeLi[2].className = 'now-page'
-					pageChangeUl.style.transition = '0.1s'
-					pageChangeUl.style.left = (pageWidth / 2) - (nowIndex + 1.5) * parseFloat(pageChangeUl.getElementsByTagName('li')[0].style.width) * unit + 'px'
 				}
 				else {
 					document.getElementById('page-content-' + hash).style.display = 'none'
@@ -82,8 +65,6 @@ var start = function () {
 					location.hash = '#' + hash
 					pageChangeLi[nowIndex].className = ''
 					pageChangeLi[nowIndex + 1].className = 'now-page'
-					pageChangeUl.style.transition = '0.1s'
-					pageChangeUl.style.left = (pageWidth / 2) - (nowIndex + 1.5) * parseFloat(pageChangeUl.getElementsByTagName('li')[0].style.width) * unit + 'px'
 				}
 				break
 			case -1:
@@ -95,8 +76,6 @@ var start = function () {
 					pageChangeLi[nowIndex].className = ''
 					pageChangeLi[1].className = 'like-page'
 					pageChangeLi[5].className = 'now-page'
-					pageChangeUl.style.transition = '0.1s'
-					pageChangeUl.style.left = (pageWidth / 2) - (nowIndex - 0.5) * parseFloat(pageChangeUl.getElementsByTagName('li')[0].style.width) * unit + 'px'
 				}
 				else {
 					document.getElementById('page-content-' + hash).style.display = 'none'
@@ -105,15 +84,9 @@ var start = function () {
 					location.hash = '#' + hash
 					pageChangeLi[nowIndex].className = ''
 					pageChangeLi[nowIndex - 1].className = 'now-page'
-					pageChangeUl.style.transition = '0.1s'
-					pageChangeUl.style.left = (pageWidth / 2) - (nowIndex - 0.5) * parseFloat(pageChangeUl.getElementsByTagName('li')[0].style.width) * unit + 'px'
 				}
 				break
 		}
-	}
-	var dataImage = document.getElementsByClassName('data-image');
-	for (var i = 0; i < dataImage.length; i++) {
-		dataImage[i].style.backgroundImage = 'url(' + dataImage[i].getAttribute('data-src') + ')';
 	}
 
 	function showMengBan(page, list) {
